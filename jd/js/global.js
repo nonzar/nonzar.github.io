e9f132c6c206617c20b1507897f10e39e5db26ca
dwljia <dwljia@qq.com>
2020-01-01 15:12:46 +0800

    feat

--- jd/js/global.js
+++ jd/js/global.js
@@ -19,9 +19,9 @@ let userList = [
 ]
 
 userList.map(e => {
-  e.qq = `https://uua.jr.jd.com/uc-fe-wxgrowing/1111jingtie/index/157433679875950707/?utm_term=qqfriends&channelLv=zhcfloating&utm_medium=jrappshare&shareId=${encodeURIComponent(e.id)}&utm_source=iOS%2Aurl%2A1574336800031`
-  e.qq2 = `https://a.ua.jd.com/uc-fe-wxgrowing/1111jingtie/index/157709390728453272/?shareId=${encodeURIComponent(e.id)}`
-  e.wechat = `https://u2.jr.jd.com/downloadApp/index.html?jumpUrl=https%3A%2F%2Fuua.jr.jd.com%2Fuc-fe-wxgrowing%2F1111jingtie%2Findex%2F%3FchannelLv%3Dzhcfloating%26shareId%3D${encodeURIComponent(e.id)}`
+  e.qq = `https://uua.jr.jd.com/uc-fe-wxgrowing/1111jingtiecommand/index/157433679875950707/?utm_term=qqfriends&channelLv=zhcfloating&utm_medium=jrappshare&shareId=${encodeURIComponent(e.id)}&utm_source=iOS%2Aurl%2A1574336800031`
+  e.qq2 = `https://a.ua.jd.com/uc-fe-wxgrowing/1111jingtiecommand/index/157709390728453272/?shareId=${encodeURIComponent(e.id)}`
+  e.wechat = `https://u2.jr.jd.com/downloadApp/index.html?jumpUrl=https%3A%2F%2Fuua.jr.jd.com%2Fuc-fe-wxgrowing%2F1111jingtiecommand%2Findex%2F%3FchannelLv%3Dzhcfloating%26shareId%3D${encodeURIComponent(e.id)}`
 })
 console.log(userList)
 
